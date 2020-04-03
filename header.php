7a7d5629f62a37f83eb687180ee879681164744b
Andre Kley <gigakley@gmail.com>
2020-04-03 03:08:48 -0300

    Inclusao no wp

---
+++ header.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="#">
+    <link rel="icon" type="image/png" href="#">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Colégio Classe A - Materiais </title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+    <link href="http://localhost:8080/wp-content/themes/materiais-centro/assets/css/bootstrap.min.css" rel="stylesheet" />
+   <link href="http://localhost:8080/wp-content/themes/materiais-centro/assets/css/light-bootstrap-dashboard.css?v=2.0.0 " rel="stylesheet" />
+   <link href="http://localhost:8080/wp-content/themes/materiais-centro/assets/css/demo.css" rel="stylesheet" />
+  <link href="http://localhost:8080/wp-content/themes/materiais-centro/assets/css/style.css" rel="stylesheet" />
+    <script src="http://localhost:8080/wp-content/themes/materiais-centro/assets/js/vue.js"></script>
+
+</head>
--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 
 <!DOCTYPE html>
 
-<html lang="en">
 
-<head>
-    <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="#">
-    <link rel="icon" type="image/png" href="#">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Colégio Classe A - Materiais </title>
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="assets/css/light-bootstrap-dashboard.css?v=2.0.0 " rel="stylesheet" />
-    <link href="assets/css/demo.css" rel="stylesheet" />
-    <link href="assets/css/style.css" rel="stylesheet" />
-    <script src="assets/js/vue.js"></script>
 
-</head>
+<?php get_header();?>
+
 
 <body>
     <div id="app">
@@ -309,18 +295,6 @@
             </script> 
 
 </body>
-<!--   Core JS Files   -->
 
-<script src="assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
-<script src="assets/js/core/popper.min.js" type="text/javascript"></script>
-<script src="assets/js/core/bootstrap.min.js" type="text/javascript"></script>
-<script src="assets/js/plugins/bootstrap-switch.js"></script>
+  <?php get_footer();?>
 
-<script src="assets/js/plugins/bootstrap-notify.js"></script>
-<!-- Control Center for Light Bootstrap Dashboard: scripts for the example pages etc -->
-<script src="assets/js/light-bootstrap-dashboard.js?v=2.0.0 " type="text/javascript"></script>
-<!-- Light Bootstrap Dashboard DEMO methods, don't include it in your project! -->
-<script src="assets/js/demo.js"></script>
-
-
-</html>
