905f368a67e0d6b13d4a837f9e91143f30b8c328
Andre Kley <gigakley@gmail.com>
2020-04-03 03:16:34 -0300

    alteracao de link

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <!--   Core JS Files   -->
 
-<script src="http://localhost:8080/wp-content/themes/materiais-centro/assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
-<script src="http://localhost:8080/wp-content/themes/materiais-centro/assets/js/core/popper.min.js" type="text/javascript"></script>
-<script src="http://localhost:8080/wp-content/themes/materiais-centro/assets/js/core/bootstrap.min.js" type="text/javascript"></script>
-<script src="http://localhost:8080/wp-content/themes/materiais-centro/assets/js/plugins/bootstrap-switch.js" type="text/javascript"></script>
-<script src="http://localhost:8080/wp-content/themes/materiais-centro/assets/js/plugins/bootstrap-notify.js" type="text/javascript"></script>
-<script src="http://localhost:8080/wp-content/themes/materiais-centro/assets/js/light-bootstrap-dashboard.js?v=2.0.0 " type="text/javascript"></script>
-<script src="http://localhost:8080/wp-content/themes/materiais-centro/assets/js/demo.js"></script>
+<script src="https://colegioclassea.com.br/wp-content/themes/materiais-centro/assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="https://colegioclassea.com.br/wp-content/themes/materiais-centro/assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="https://colegioclassea.com.br/wp-content/themes/materiais-centro/assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+<script src="https://colegioclassea.com.br/wp-content/themes/materiais-centro/assets/js/plugins/bootstrap-switch.js" type="text/javascript"></script>
+<script src="https://colegioclassea.com.br/wp-content/themes/materiais-centro/assets/js/plugins/bootstrap-notify.js" type="text/javascript"></script>
+<script src="https://colegioclassea.com.br/wp-content/themes/materiais-centro/assets/js/light-bootstrap-dashboard.js?v=2.0.0 " type="text/javascript"></script>
+<script src="https://colegioclassea.com.br/wp-content/themes/materiais-centro/assets/js/demo.js"></script>
 
 
 </html>
--- header.php
+++ header.php
@@ -12,10 +12,10 @@
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
-    <link href="http://localhost:8080/wp-content/themes/materiais-centro/assets/css/bootstrap.min.css" rel="stylesheet" />
-   <link href="http://localhost:8080/wp-content/themes/materiais-centro/assets/css/light-bootstrap-dashboard.css?v=2.0.0 " rel="stylesheet" />
-   <link href="http://localhost:8080/wp-content/themes/materiais-centro/assets/css/demo.css" rel="stylesheet" />
-  <link href="http://localhost:8080/wp-content/themes/materiais-centro/assets/css/style.css" rel="stylesheet" />
-    <script src="http://localhost:8080/wp-content/themes/materiais-centro/assets/js/vue.js"></script>
+    <link href="https://colegioclassea.com.br/wp-content/themes/materiais-centro/assets/css/bootstrap.min.css" rel="stylesheet" />
+   <link href="https://colegioclassea.com.br/wp-content/themes/materiais-centro/assets/css/light-bootstrap-dashboard.css?v=2.0.0 " rel="stylesheet" />
+   <link href="https://colegioclassea.com.br/wp-content/themes/materiais-centro/assets/css/demo.css" rel="stylesheet" />
+  <link href="https://colegioclassea.com.br/wp-content/themes/materiais-centro/assets/css/style.css" rel="stylesheet" />
+    <script src="https://colegioclassea.com.br/wp-content/themes/materiais-centro/assets/js/vue.js"></script>
 
 </head>
